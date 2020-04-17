1c73cb70a318083c9f49e29ffb8b5f280ca0632a
Chibuisi Enyia <enyia21@gmail.com>
2020-04-17 10:28:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badge_messages = []
+  names.each do |name|
+    badge_messages << badge_maker(name)
+   end
+   return batch_messages
+end
